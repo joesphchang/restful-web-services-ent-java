bddab4789e2e7e3cf1ca8d788ab1bc8457ae53ea
joesphchang <joesphschang@gmail.com>
2026-03-12 19:09:30 -0500

    forgot to delete log4j imports

--- src/main/java/com/joeychang/metmuseum/MetMuseumDao.java
+++ src/main/java/com/joeychang/metmuseum/MetMuseumDao.java
@@ -3,8 +3,6 @@ package com.joeychang.metmuseum;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joeychang.persistence.MetMuseum;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
